c2be8d772c086a170377ad900cac9ca3543e36e8
Ritanshu17 <ritanshu755mankar@gmail.com>
2026-02-14 21:45:22 +0530

    the main API scripts

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ from fastapi import FastAPI
 from intent_engine import extract_intent
 from guardrails import validate_intent
 from response_engine import build_response
+
+app = FastAPI()
+
+@app.post("/generate-reply")
+
+def generate_reply(message: str):
+    intent = extract_intent(query)
+    validate_intent(intent)
+    response = build_response(intent)
+
+    return{
+        "intent": intent,
+        "reply": response
+    } 
